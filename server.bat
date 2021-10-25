60a20eb1fcbf370ec000068f537909d93dbbb82f
Stefan Boronczyk <stefan.amin.boronczyk@googlemail.com>
2021-10-25 20:44:50 +0200

    add path

--- server.bat
+++ server.bat
@@ -5,5 +5,5 @@ set batdir=%~dp0
 :: --MISSING ENDING BACKSLASH
 :: set batdir=%CD%
 pushd "%batdir%"
-dotnet msbuild "C:/Game/project/FPS.sln" /restore /t:Build /p:Configuration=Debug /v:normal /p:GodotTargetPlatform=windows
+dotnet msbuild ./project/FPS.sln /restore /t:Build /p:Configuration=Debug /v:normal /p:GodotTargetPlatform=windows
 editor\godot.windows.opt.tools.64.mono.exe --path ./project -server
